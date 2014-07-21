4fefdb52e3a2859cefbe10d57ba378be12b228ff
Ramoonus <Ramoonus@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-07-21 08:16:13 +0000

    tag v3.7.2
    git-svn-id: https://plugins.svn.wordpress.org/html5shiv/tags/3.7.2@952034 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ html5shiv.php
@@ -0,0 +1,21 @@
+<?php
+/*
+Plugin Name: HTML5shiv
+Plugin URI: http://www.ramoonus.nl/wordpress/html5shiv/
+Description: The latest HTML5 JavaScript shiv for IE to recognise and style the HTML5 elements. 
+Version: 3.7.2
+Author: Ramoonus
+Author URI: http://www.ramoonus.nl/
+*/
+
+function rw_html5shiv() {
+	
+		// Original
+		wp_deregister_script('html5shiv'); // deregister
+		//wp_enqueue_script('html5shiv', plugins_url('/js/html5shiv.min.js', __FILE__), false, '3.7.2');
+		
+		// With Printshiv
+		wp_deregister_script('html5shiv-printshiv'); // deregister
+		wp_enqueue_script('html5shiv-printshiv', plugins_url('/js/html5shiv-printshiv.min.js', __FILE__), false, '3.7.2');
+}
+?>
